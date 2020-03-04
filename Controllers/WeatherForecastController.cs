a50f38d07aa6609e05294dfdc1856499150a4b24
Chris Wiederspan <chris@wiederspan.com>
2020-03-03 17:10:25 -0700

    Minor changes

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -20,7 +20,7 @@ namespace AadProtectedApi.Controllers {
         };
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger) {
-            _logger = logger;
+            this._logger = logger;
         }
 
         [HttpGet]
